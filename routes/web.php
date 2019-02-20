d9f8edf880b4a4e41b18b9c6f6b7fe28a9ad69ac
victorkiambi <kiambi15@gmail.com>
2019-02-20 13:56:28 +0300

    looking good

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,33 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    $links = \App\Link::all();
+
+    return view('welcome', ['links' => $links]);
+});
+
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
+});
